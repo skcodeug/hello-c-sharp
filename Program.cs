d67a93457331dc2783aa13bf1de9977b8978ed76
skcodeug <60166232+skcodeug@users.noreply.github.com>
2022-12-06 16:32:33 +0300

    deeper understanding of console template along with debugging

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+var processList = Process.GetProcesses();
+var process = processList.Where(p=> p.ProcessName == "dotnet").First();
+Console.WriteLine("Processing your feelings is " + process.PriorityClass);
 Console.WriteLine("Hello, World!");
 Console.WriteLine("The current time is " + DateTime.Now);
